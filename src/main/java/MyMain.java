82a90e8568d35382f8b98df454e39a68e0ff0961
Rohan <rohanzealous@gmail.com>
2021-09-08 14:21:02 -0700

    finish

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,32 +1,45 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MyMain {
-    // Write a method that asks the user to input a double between 0.0 and 1000000.0
+    // Write a method that asks the user to input a double between 0.0 and 1,000,000.0
     // Your method should keep asking them until they type in a valid value
     // The method returns the final double they type in
 
     // scan.nextDouble() might be useful here
     public static double inputDouble() {
         Scanner scan = new Scanner(System.in);
-        // REPLACE THIS WITH YOUR CODE
-
-        return -1.0;
+        double x = scan.nextDouble();
+        while (x > 1_000_000.0 || x < 0.0) {
+            System.out.println("gimme an eligible double");
+            x = scan.nextDouble();
+        }
+        return x;
     }
 
     // Takes a double money as input and returns the number of quarters that we
     // can return in change
     // You should use a while loop!
     public static int numQuarters(double money) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int x =0;
+        while (money >= .25) {
+            money -= 0.25;
+            x += 1;
+        }
+        return x;
+        // return (int) ((int) money/0.25);
     }
 
     // Takes a double money as input and returns the number of dimes that we
     // can return in change
     // You should use a while loop!
     public static int numDimes(double money) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int x = 0;
+        while (money >= .10) {
+            money -= 0.10;
+            x += 1;
+        }
+        return x;
     }
 
 
@@ -36,7 +49,9 @@ public class MyMain {
     //  Takes in two Strings as input and returns a String equal to those two Strings added
     //  together in alphabetic order.
     public static String stringOrder(String str1, String str2) {
-        return "";
+        char str[] = (str1 + str2).toCharArray();
+        Arrays.sort(str);
+        return new String(str);
     }
 
 
